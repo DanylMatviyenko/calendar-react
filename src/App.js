aff3c14e5f76cb23b218a402c37d9ff4a87017b2
DanylMatviyenko <dan.matvienko13@gmail.com>
2021-01-12 17:49:20 +0200

    MonthSwitcher component was created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { MonthSwitcher } from 'Components/MonthSwitcher'
 import lastDayOfMonth from 'date-fns/lastDayOfMonth';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.lastDayOfCurrentMonth = lastDayOfMonth(new Date());
+    this.state = {
+      lastDayOfCurrentMonth: lastDayOfMonth(new Date())
+    }
+  }
+  updateLastDayOfCurrentMonth = (newLastDay) => {
+    this.setState({
+      lastDayOfCurrentMonth: newLastDay
+    });
   }
   render() {
-    return null;
+    return (
+      <MonthSwitcher lastDayOfCurrentMonth={ this.state.lastDayOfCurrentMonth }
+                     updateDate={ this.updateLastDayOfCurrentMonth }/>
+    )
   }
 }
 
